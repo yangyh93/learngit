9fac11e0ec10b40806233ee11c045f4247f358f5
yangyonghong <yangyonghong1993@126.com>
2018-10-30 17:51:28 +0800

    dev add hello-world

---
+++ hello-world.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Hello
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("hello world");
+			Console.ReadKey();
+		}
+	}
+}
+
